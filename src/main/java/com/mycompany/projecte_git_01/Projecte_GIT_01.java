5baa1e1cfb1a57a93868d7aad2a00b47a3ccc257
lluc <lluc.rasfib.dam@insalfonscostafreda.cat>
2023-12-11 21:50:41 +0100

    Pujada_tres_instancia_cridaMetode

--- src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
+++ src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
@@ -6,5 +6,12 @@ public class Projecte_GIT_01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        //creem una instància de Class_Persona
+        Class_Persona Persona = new Class_Persona ("Pere", 25);
+        
+        //Crida el métode saludar a NewClass
+        Persona.saludar();
+        
     }
 }
