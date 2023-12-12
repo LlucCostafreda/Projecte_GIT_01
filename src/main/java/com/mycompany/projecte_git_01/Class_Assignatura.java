0eaaa2d315fee72534734bdfbdfd0cde3395fc9d
lluc <lluc.rasfib.dam@insalfonscostafreda.cat>
2023-12-12 20:45:08 +0100

    entrada1_Branca2

--- src/main/java/com/mycompany/projecte_git_01/Class_Assignatura.java
+++ src/main/java/com/mycompany/projecte_git_01/Class_Assignatura.java
@@ -2,8 +2,18 @@ package com.mycompany.projecte_git_01;
 
 public class Class_Assignatura {
 
-    public static void main(String[] args) {
-        System.out.println("Hola soc Lluc");
+    private final String assignatura;
+    private final int nota;
 
+    //constructor
+    public Class_Assignatura(String assignatura, int nota) {
+        this.assignatura = assignatura;
+        this.nota = nota;
     }
+
+//Metode indicar la nota
+    public void lesNotes() {
+        System.out.println("Hola, l'assigntura es:  " + assignatura + " i la teva nota es un: " + nota + " enhorabona.");
+    }
+//Altres atributs o metodoes afegir aqui
 }
--- src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
+++ src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
@@ -1,17 +1,20 @@
-
 package com.mycompany.projecte_git_01;
-
 
 public class Projecte_GIT_01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+
         //creem una instància de Class_Persona
-        Class_Persona Persona = new Class_Persona ("Pere", 25);
-        
+        Class_Persona Persona = new Class_Persona("Pere", 25);
+
         //Crida el métode saludar a NewClass
         Persona.saludar();
-        
+
+        //creem una instància de Class_Persona
+        Class_Assignatura Assignatura = new Class_Assignatura("M5.Entorns Desenvolupament", 10);
+
+        //Crida el métode saludar a NewClass
+        Assignatura.lesNotes();
     }
 }
